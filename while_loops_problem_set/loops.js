f56a9e9b3cb288ccaa6c2ca9b795fa1d4b8454e7
Velimir Kumric <vel.kumric@gmail.com>
2017-02-11 16:12:20 -0500

    Added final 2 answers.

--- while_loops_problem_set/loops.js
+++ while_loops_problem_set/loops.js
@@ -16,3 +16,28 @@ while (problem_2 < 41)
 	console.log(problem_2);
 	problem_2 += 2;
 }
+
+//Print odd numbers between 300 & 333
+
+var odd_num = 301;
+
+while (odd_num < 334)
+{
+	console.log(odd_num)
+	odd_num += 2;
+}
+
+// Print all numbers divisible by 5 and 3 between 5 and 50
+
+var divisible = 5;
+
+while (divisible < 51)
+	if (divisible % 5 === 0 && divisible % 3 === 0)
+	{
+		console.log(divisible)
+		divisible++;
+	}
+	else
+	{
+		divisible++;
+	}
