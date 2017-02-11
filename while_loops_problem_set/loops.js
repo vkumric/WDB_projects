af0ecd739f5515bd174ec84fb014989feac52d9b
Velimir Kumric <vel.kumric@gmail.com>
2017-02-11 16:01:16 -0500

    Completed problem 1 & 2

--- while_loops_problem_set/loops.js
+++ while_loops_problem_set/loops.js
@@ -1,1 +1,18 @@
-alert("CONNECTED");
+// Print all numbers from -10 to 19
+var problem_1 = -10;
+
+while (problem_1 < 20)
+{
+	console.log(problem_1);
+	problem_1++;
+} 
+
+//Print even numbers between 10 to 40.
+
+var problem_2 = 10;
+
+while (problem_2 < 41)
+{
+	console.log(problem_2);
+	problem_2 += 2;
+}
